ff680b855c480ea55a2a93718b51d1f87d9dbe30
James Duda <jimbojim1997@gmail.com>
2023-04-22 23:00:48 +0100

    Add solution and project with KeePass2 reference and build paths.

---
+++ SsmsLogin/SsmsLoginExt.cs
@@ -0,0 +1,22 @@
+﻿using KeePass.Plugins;
+
+namespace SsmsLogin
+{
+    /// <summary>
+    /// A KeePass2 extension for logging into Microsoft SQL Server Management Studio.
+    /// <para />
+    /// KeePass2 documentation: https://keepass.info/help/v2_dev/plg_index.html
+    /// </summary>
+    public sealed class SsmsLoginExt : Plugin
+    {
+        private IPluginHost _host;
+
+        public override bool Initialize(IPluginHost host)
+        {
+            if (host is null) return false;
+            base.Initialize(host);
+            _host = host;
+            return true;
+        }
+    }
+}
